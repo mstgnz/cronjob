88e943e6f982c2314a5a596b70df5f1225e185a5
mstgnz <genezmesut@gmail.com>
2024-03-29 09:18:17 +0300

    template

--- handler/home.go
+++ handler/home.go
@@ -14,29 +14,3 @@ func (h *Home) HomeHandler(w http.ResponseWriter, _ *http.Request) {
 		log.Println(err)
 	}
 }
-
-func PostHandler(w http.ResponseWriter, _ *http.Request) {
-	if err := config.Render(w, "post", map[string]any{}); err != nil {
-		log.Println(err)
-	}
-}
-
-func CreateHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		if err := config.Render(w, "create", map[string]any{}); err != nil {
-			log.Println(err)
-		}
-	} else {
-		title := r.Form.Get("title")
-		fullname := r.Form.Get("fullname")
-		content := r.Form.Get("content")
-
-		if title != "" && fullname != "" && content != "" {
-
-			w.Write([]byte("success"))
-		} else {
-			w.Write([]byte("invalid form"))
-		}
-	}
-
-}
