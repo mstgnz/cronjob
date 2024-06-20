c6d9256a1f67959eca64fe3edddb6ffd58f11e85
Mesut GENEZ <genezmesut@gmail.com>
2024-06-20 14:05:40 +0300

    webhook componenets

--- handler/web/webhook.go
+++ handler/web/webhook.go
@@ -9,5 +9,5 @@ import (
 type WebhookHandler struct{}
 
 func (h *WebhookHandler) HomeHandler(w http.ResponseWriter, r *http.Request) error {
-	return config.Render(w, "webhook", map[string]any{})
+	return config.Render(w, "webhook", map[string]any{}, "webhook/list", "webhook/new")
 }
