2b8c6a884896f83677b0e3fdcffe99ecaab54b1b
Mesut GENEZ <genezmesut@gmail.com>
2024-06-24 11:08:45 +0300

    app log

--- models/app_log.go
+++ models/app_log.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/mstgnz/cronjob/config"
@@ -11,6 +13,42 @@ type AppLog struct {
 	Level     string     `json:"level"`
 	Message   string     `json:"message"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
+}
+
+func (m *AppLog) Get(offset, limit int, level string) ([]AppLog, error) {
+
+	query := strings.TrimSuffix(config.App().QUERY["APP_LOG_PAGINATE"], ";")
+
+	if level != "" {
+		query += fmt.Sprintf(" AND level=%s", level)
+	}
+
+	// prepare
+	stmt, err := config.App().DB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	// query
+	rows, err := stmt.Query(offset, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = stmt.Close()
+		_ = rows.Close()
+	}()
+
+	var appLogs []AppLog
+	for rows.Next() {
+		var appLog AppLog
+		if err := rows.Scan(&appLog.ID, &appLog.Level, &appLog.Message, &appLog.CreatedAt); err != nil {
+			return nil, err
+		}
+		appLogs = append(appLogs, appLog)
+	}
+
+	return appLogs, nil
 }
 
 func (m *AppLog) Create(level, message string) error {
