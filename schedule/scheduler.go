62e55baf58d8abfff5ebb081cc2cf82a94191961
Mesut GENEZ <genezmesut@gmail.com>
2024-07-07 23:57:56 +0300

    scheduler

--- schedule/scheduler.go
+++ schedule/scheduler.go
@@ -9,16 +9,6 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-// This application utilizes a database locking mechanism to prevent duplicate cronjob tasks
-// when running multiple instances in a Kubernetes (k8s) environment.
-
-// The instance that first adds a record to the schedule_logs table will be the one triggering
-// the cronjob task. Subsequent instances won't be able to create a new record due to an existing one,
-// resulting in an error and preventing them from executing the task.
-// The microsecond differences between instances will automatically facilitate the locking mechanism.
-
-// Nevertheless, it is strongly advised for users of this project to establish an additional control
-// mechanism on their own systems.
 func CallSchedule(c *cron.Cron) {
 	// set location
 	loc, err := time.LoadLocation("Europe/Istanbul")
@@ -31,7 +21,7 @@ func CallSchedule(c *cron.Cron) {
 	// Alotech Call - every night at 23:59
 	if _, err = c.AddFunc("* * * * *", func() {
 		config.ShuttingWrapper(func() {
-			AlotechCall()
+			// todo
 		})
 
 	}); err != nil {
